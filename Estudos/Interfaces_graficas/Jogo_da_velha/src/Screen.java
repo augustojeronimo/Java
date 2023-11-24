bf1972702aee673afe8c1a11fd62fc4d39d641c9
Augusto Jerônimo <dev.augustoj@gmail.com>
2023-11-24 17:49:38 -0300

    Update Screen.java

--- Estudos/Interfaces_graficas/Jogo_da_velha/src/Screen.java
+++ Estudos/Interfaces_graficas/Jogo_da_velha/src/Screen.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class Screen extends JFrame {
@@ -28,22 +29,71 @@ public class Screen extends JFrame {
         fieldPanel.setBackground(FIELD_COLOR);
         fieldPanel.setBounds((WIDTH/2)-(HEIGHT/2),1,HEIGHT,HEIGHT);
 
+        fieldPanel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
         /* - Botões - */
 
-        button1 = new JButton();
-        button2 = new JButton();
-        button3 = new JButton();
-        button4 = new JButton();
-        button5 = new JButton();
-        button6 = new JButton();
-        button7 = new JButton();
-        button8 = new JButton();
-        button9 = new JButton();
+        int buttonSize = fieldPanel.getHeight()/3 - 4; // Tamanho do botão
+        int[] pos = {0, fieldPanel.getHeight()/2 - buttonSize/2, fieldPanel.getHeight() - buttonSize};
 
-        /* -  - */
+        buttons = new JButton[3][3];
+
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+
+                buttons[l][c] = new JButton();
+
+                buttons[l][c].setBounds(pos[l],pos[c],buttonSize,buttonSize);
+
+                buttons[l][c].setBackground(SPOT_COLOR);
+                buttons[l][c].setBorder(new LineBorder(new Color(0), 0));
+
+                fieldPanel.add(buttons[l][c]);
+
+            }
+        }
+
+        /* - Ação dos botões - */
+
+
+
+        for (int l = 0; l < 3; l++) {
+            for (int c = 0; c < 3; c++) {
+
+                final int y = l;
+                final int x = c;
+
+                buttons[l][c].addActionListener(e -> {
+
+                    if (buttons[y][x].getText().isEmpty()) {
+
+                        buttons[y][x].setText(jogador);
+
+                        switch (jogador){
+                            case "X":
+                                jogador = "O";
+                                break;
+                            case "O":
+                                jogador = "X";
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+
+                    System.out.println(jogador);
+                });
+
+            }
+        }
 
         /* -  - */
     }
+
+    /* Jogo */
+
+    private String jogador;
 
     /* Medidas */
     private final int HEIGHT = 500;
@@ -51,23 +101,16 @@ public class Screen extends JFrame {
 
     /* Cores */
 
-    private Color BACKGROUND_COLOR = new Color(100,100,200);
-    private Color FIELD_COLOR = new Color(8, 28, 59);
+    private final Color BACKGROUND_COLOR = new Color(100,100,200);
+    private final Color FIELD_COLOR = new Color(0,0,0);
+    private final Color SPOT_COLOR = new Color(15, 42, 110);
 
     /* Elementos */
 
     private JPanel backgroundPanel;
     private JPanel fieldPanel;
 
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
+    private JButton[][] buttons;
 
     public static void main(String[] args) {
         Screen game = new Screen();
